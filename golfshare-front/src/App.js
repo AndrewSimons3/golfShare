42d5a2c04d92d17d8ebd677c3d6f562899e6037e
AndrewSimons3 <simdrew07@gmail.com>
2022-04-19 07:29:13 -0500

    set up routing

--- golfshare-front/src/App.js
+++ golfshare-front/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Users />} />
+				<Route path='*' element={<Navigate to='/' replace />} />
+				<Route path='/places/new' element={<NewPlace />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
