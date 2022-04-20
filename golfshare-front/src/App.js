b29528c4d0b05ff1c97532839357fe64fc51739c
AndrewSimons3 <simdrew07@gmail.com>
2022-04-20 08:40:17 -0500

    added side drawer plus header plus nav links

--- golfshare-front/src/App.js
+++ golfshare-front/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Users />} />
-				<Route path='*' element={<Navigate to='/' replace />} />
-				<Route path='/places/new' element={<NewPlace />} />
-			</Routes>
+			<MainNavigation />
+			<main>
+				<Routes>
+					<Route path='/' element={<Users />} />
+					<Route path='*' element={<Navigate to='/' replace />} />
+					<Route path='/places/new' element={<NewPlace />} />
+				</Routes>
+			</main>
 		</BrowserRouter>
 	);
 }
--- golfshare-front/src/user/components/UsersList.js
+++ golfshare-front/src/user/components/UsersList.js
@@ -1,19 +1,21 @@
-
-import './UsersList.css';
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
 const UsersList = (props) => {
  
   if (props.items.length === 0) {
     return (
-      <div className="center">
-        <h2>No users found.</h2>
-      </div>
-    );
+			<div className='center'>
+				<Card>
+					<h2>No users found.</h2>
+				</Card>
+			</div>
+		);
   }
   
   return (
-    <ul>
+    <ul className="users-list">
       {props.items.map(user => (
         <UserItem
           key={user.id}
