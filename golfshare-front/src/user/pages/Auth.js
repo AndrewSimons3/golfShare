a7d9e38c436a3b0aa4f270394ef03bb4f9882bb1
AndrewSimons3 <simdrew07@gmail.com>
2022-05-07 08:07:35 -0500

    updated loading and error states

--- golfshare-front/src/user/pages/Auth.js
+++ golfshare-front/src/user/pages/Auth.js
@@ -7,14 +7,18 @@ import {
 	VALIDATOR_MINLENGTH,
 	VALIDATOR_REQUIRE,
 } from '../../shared/util/validators';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Auth = () => {
 	const auth = useContext(AuthContext);
 
 	const [isLoginMode, setIsLoginMode] = useState(true);
+	const [error, setError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const [formState, inputHandler, setFormData] = useForm(
 		{
@@ -30,9 +34,36 @@ const Auth = () => {
 		false
 	);
 
-	const authSubmitHandler = (event) => {
+	const authSubmitHandler = async (event) => {
 		event.preventDefault();
-		console.log(formState.inputs);
+
+		if (isLoginMode) {
+		} else {
+			try {
+				setIsLoading(true);
+				const response = await fetch('http://localhost:5000/api/users/signup', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						name: formState.inputs.name.value,
+						email: formState.inputs.email.value,
+						password: formState.inputs.password.value,
+					}),
+				});
+
+				const responseData = await response.json();
+				console.log(responseData);
+				setIsLoading(false);
+			} catch (err) {
+				console.log(err);
+				setIsLoading(false);
+				setError(err.message || 'Something went wrong, please try again.');
+			}
+		}
+		
+
 		auth.login();
 	};
 
@@ -62,6 +93,7 @@ const Auth = () => {
 
 	return (
 		<Card className='authentication'>
+			{isLoading && <LoadingSpinner asOverlay/>}
 			<h2>Login Required</h2>
 			<hr />
 			<form onSubmit={authSubmitHandler}>
